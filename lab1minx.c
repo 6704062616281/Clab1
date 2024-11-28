0905e12f9e10eb75d3aa94a68a7ba5b3ce9d3af3
Warisara0419 <s6704062616281@email.kmutnb.ac.th>
2024-11-28 18:24:51 +0700

    Shorter code

--- lab1minx.c
+++ lab1minx.c
@@ -2,10 +2,8 @@
 int main(){
 	int a;
 	int b;
-	printf("number1 is: ");
-	scanf("%d",&a);
-	printf("number2 is: ");
-	scanf("%d",&b);
+	printf("number1 and number2: ");
+	scanf("%d %d", &a, &b);
 	if (a>b){
 		printf("a in greater than b");
 		//diff of a and b
