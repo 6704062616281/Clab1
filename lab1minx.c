2e4dcd168a3d1a31c31f3466fc470d96c2a7bc7a
Warisara0419 <s6704062616281@email.kmutnb.ac.th>
2024-11-28 18:15:39 +0700

    new v input

--- lab1minx.c
+++ lab1minx.c
@@ -1,9 +1,17 @@
+#include <stdio.h>
 int main(){
-	int a=10;
-	int b=50;
+	int a;
+	int b;
+	printf("number1 is: ");
+	scanf("%d",&a);
+	printf("number2 is: ");
+	scanf("%d",&b);
 	if (a>b){
 		printf("a in greater than b");
+		//diff of a and b
+		printf("\n%d",(a-b));
 	} else {
 		printf("b in greater than a");
+		printf("\n%d",(b-a));
 	}
 }
